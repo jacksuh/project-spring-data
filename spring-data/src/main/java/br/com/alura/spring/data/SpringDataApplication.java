5075f9274fb8aa2a9b3f2107b17ef5005bbbf3c3
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-02-04 16:27:39 -0200

    Project Spring-data

---
+++ spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -0,0 +1,78 @@
+package br.com.alura.spring.data;
+
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import br.com.alura.spring.data.service.CrudCargoService;
+import br.com.alura.spring.data.service.CrudFuncionarioService;
+import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
+import br.com.alura.spring.data.service.RelatoriosService;
+
+@EnableJpaRepositories
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner {
+
+	private Boolean system = true;
+
+	private final CrudCargoService cargoService;
+
+	private final RelatoriosService relatoriosService;
+	
+	private final CrudFuncionarioService funcionarioService;
+
+	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+
+	public SpringDataApplication(CrudCargoService cargoService, 
+			RelatoriosService relatoriosService, 
+			CrudFuncionarioService funcionarioService, 
+			CrudUnidadeTrabalhoService unidadeTrabalhoService) {
+		this.cargoService = cargoService;
+		this.relatoriosService = relatoriosService;
+		this.funcionarioService = funcionarioService;
+		this.unidadeTrabalhoService = unidadeTrabalhoService;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+
+		while (system) {
+			System.out.println("Qual função deseja executar?");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+			System.out.println("2 - Funcionario");
+			System.out.println("3 - Unidade");
+			System.out.println("4 - Relatorios");
+			
+
+			Integer function = scanner.nextInt();
+
+			switch (function) {
+				case 1:
+					cargoService.inicial(scanner);
+					break;
+				case 2:
+					funcionarioService.inicial(scanner);
+					break;
+				case 3:
+					unidadeTrabalhoService.inicial(scanner);
+					break;
+				case 4:
+					relatoriosService.inicial(scanner);
+					break;
+				default:
+					System.out.println("Finalizando");
+					system = false;
+					break;
+			}
+		}
+	}
+}
---
+++ spring-data/src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -0,0 +1,23 @@
+package br.com.alura.spring.data.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.spring.data.orm.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
+	List<Funcionario> findByNome(String nome);
+	
+	@Query("SELECT f FROM Funcionario f WHERE f.nome = :nome "
+			+ "AND f.salario >= :salario AND f.dataContratacao = :data")
+	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate data);
+	
+	@Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data",
+			nativeQuery = true)
+	List<Funcionario> findDataContratacaoMaior(LocalDate data);
+}
